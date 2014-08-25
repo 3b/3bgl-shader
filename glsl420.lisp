06d07115fbbbbac17551e0ae5ddabba7e3011b8a
Bart Botta <00003b@gmail.com>
2014-08-25 06:44:08 -0500

    update built-in variables/constants

--- glsl420.lisp
+++ glsl420.lisp
@@ -11,6 +11,12 @@
                `(3bgl-shaders::walk '(input ,name ,type :stage ,stage
                                                        :internal t)
                                     walker))
+             ;; alias for INPUT for now
+             (const (name type stage &rest qualifiers)
+               (declare (ignorable qualifiers))
+               `(3bgl-shaders::walk '(input ,name ,type :stage ,stage
+                                      :internal t)
+                                    walker))
              (output (name type stage  &rest qualifiers)
                (declare (ignorable qualifiers))
                `(3bgl-shaders::walk '(output ,name ,type :stage ,stage
@@ -21,6 +27,16 @@
                                         ,@slots)
                    walker)))
 
+    ;; compute (430+)
+    (input (gl-num-work-groups "gl_NumWorkGroups") :uvec3 :compute)
+    (const (gl-work-group-size "gl_WorkGroupSize") :uvec3 :compute)
+    (input (gl-work-group-id "gl_WorkGroupID") :uvec3 :compute)
+    (input (gl-local-invocation-id "gl_LocalInvocationID") :uvec3 :compute)
+    (input (gl-global-invocation-id "gl_GlobalInvocationID") :uvec3 :compute)
+    (input (gl-local-invocation-index "gl_LocalInvocationIndex") :int :compute) 
+
+
+    ;; vertex
     (input (gl-vertex-id "gl_VertexID") :int :vertex)
     (input (gl-instance-id "gl_InstanceID") :int :vertex)
     (interface (gl-per-vertex "gl_PerVertex")
@@ -38,44 +54,44 @@
       ((gl-position "gl_Position") :vec4)
       ((gl-point-size "gl_PointSize") :float)
       ((gl-clip-distance "gl_ClipDistance") (:float *)) ;; fixme: array syntax?
+      ((gl-cull-distance "gl_CullDistance") (:float *)) ;; glsl 450
       )
+
+    ;; geometry
     (input (gl-primitive-id-in "gl_PrimitiveIDIn") :int :geometry)
     (input (gl-invocation-id "gl_InvocationID") :int :geometry)
     (output (gl-primitive-id "gl_PrimitiveID") :int :geometry)
-    (output (gl-layer "gl_Layer") :int :geometry)
-    (output (gl-viewport-index "gl_ViewportIndex") :int :geometry)
+    (output (gl-layer "gl_Layer") :int :geometry) ;; 430?
+    (output (gl-viewport-index "gl_ViewportIndex") :int :geometry) ;; 430?
+
+    ;; tessellation control
     (input (gl-patch-vertices-in "gl_PatchVerticesIn") :int :tess-control)
     (input (gl-primitive-id "gl_PrimitiveID") :int :tess-control)
     (input (gl-invocation-id "gl_InvocationID") :int :tess-control)
     (output (gl-tess-level-outer "gl_TessLevelOuter") (:float 4) :tess-control :patch)
     (output (gl-tess-level-inner "gl_TessLevelInner") (:float 2) :tess-control :patch)
+
+    ;; tessellation evaluation
     (input (gl-patch-vertices-in "gl_PatchVerticesIn") :int :tess-eval)
     (input (gl-primitive-id "gl_PrimitiveID") :int :tess-eval)
     (input (gl-tess-coord "gl_TessCoord") :vec3 :tess-eval)
     (input (gl-tess-level-outer "gl_TessLevelOuter" (:float 4)) :tess-eval :patch)
     (input (gl-tess-level-inner "gl_TessLevelInner" (:float 2)) :tess-eval :patch)
 
+    ;; fragment
     (input (gl-frag-coord "gl_FragCoord") :vec4 :fragment)
     (input (gl-front-facing "gl_FrontFacing") :bool :fragment)
     (input (gl-clip-distance "gl_ClipDistance") (:float *) :fragment)
+    (input (gl-cull-distance "gl_CullDistance") (:float *) :fragment) ;; 450
     (input (gl-point-coord "gl_PointCoord") :vec2 :fragment)
     (input (gl-primitive-ID "gl_PrimitiveID") :int :fragment)
     (input (gl-sample-id "gl_SampleID") :int :fragment)
     (input (gl-sample-position "gl_SamplePosition") :vec2 :fragment)
     (input (gl-sample-mask-in "gl_SampleMaskIn") (:int *) :fragment)
+    (input (gl-layer "gl_Layer") :int :fragment) ;; 430?
+    (input (gl-viewport-index "gl_ViewportIndex") :int :fragment) ;; 430?
+    (input (gl-helper-invocation "gl_HelperInvocation") :bool :fragment) ;; 450
     (output (gl-frag-depth "gl_FragDepth") :float :fragment)
     (output (gl-sample-mask "gl_SampleMask") (:int *) :fragment)
-
-    ;; 4.30 stuff
-    ;; fixme: probably should split this out
-    (input (gl-num-work-groups "gl_NumWorkGroups") :uvec3 :compute)
-    (input (gl-work-group-size "gl_WorkGroupSize") :uvec3 :compute) ;; const instead of in?
-    (input (gl-work-group-id "gl_WorkGroupID") :uvec3 :compute)
-    (input (gl-local-invocation-id "gl_LocalInvocationID") :uvec3 :compute)
-    (input (gl-global-invocation-id "gl_GlobalInvocationID") :uvec3 :compute)
-    (input (gl-local-invocation-index "gl_LocalInvocationIndex") :uint :compute)
-
-    (input (gl-layer "gl_Layer") :int :fragment)
-    (input (gl-viewport-index "gl_ViewportIndex") :int :fragment)
 
 ))
