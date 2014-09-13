d73dd2d9eeec7cf800f802b81358dd865bfe7f6d
Bart Botta <00003b@gmail.com>
2014-09-13 06:49:49 -0500

    add an example program and some shaders

---
+++ example-shaders.lisp
@@ -0,0 +1,258 @@
+(defpackage #:3bgl-shader-example-shaders
+  (:use #:cl #:3bgl-glsl)
+  ;; shadow defun and defconstant with shader versions
+  (:shadowing-import-from #:3bgl-glsl #:defun #:defconstant)
+  (:export
+   #:minimal-vertex
+   #:minimal-fragment))
+(in-package #:3bgl-shader-example-shaders)
+
+
+;; vertex shader inputs, we specify :location so we don't have to query the
+;; attributes from host code and can just use the specified locations
+(input position :vec4 :location 0)
+
+;; if we were using modern GL, we would define some more vertex attributes
+;; (input normal :vec3 :location 1)
+
+;; for glutSolidTeapot we need to use the compatibility mode
+;; attributes, so we specify the GLSL name of the fixed-function
+;; vertex attributes instead of a location
+(input (normal "gl_Normal") :vec3)
+(input (uv "gl_MultiTexCoord0") :vec4)
+
+;; fragment shader output
+(output color :vec4 :stage :fragment)
+
+;; uniforms
+(uniform mv :mat4) ;; model-view matrix
+(uniform mvp :mat4) ;; model-view-projection matrix
+(uniform normal-matrix :mat4)
+(uniform time :float)
+
+
+;; output from vertex shader, interpolated by GL then sent as input to
+;; fragment shader
+;; visible in vertex shader as 'outs', and in fragment shader as 'ins'
+(interface varyings (:out (:vertex outs)
+                     :in (:fragment ins))
+  (position :vec4)
+  (normal :vec3)
+  (color :vec4)
+  (uv :vec4)
+  (eye-direction :vec3)
+  (light-direction :vec3))
+
+;; minimal shader program, just draw the object in solid red
+(defun minimal-vertex ()
+  ;; transform the input vertex position by the current
+  ;; modelview-projection matrix and assign it to the built-in
+  ;; GL-POSITION variable
+  (setf gl-position (* mvp position)))
+
+(defun minimal-fragment ()
+  ;; set the output color to opaque red (R G B A =  1 0 0 1)
+  (setf color (vec4 1 0 0 1)))
+
+
+
+;; bit more complex, interpolate normals, and draw those to color
+(defun normal-vertex ()
+  (setf gl-position (* mvp position))
+  ;; transform normals and send to fragment shader
+  (setf (@ outs normal) (* (mat3 normal-matrix) normal)))
+
+(defun normal-fragment ()
+  ;; just set the color to the interpolated normal, with alpha set to 1
+  (setf color (vec4 (@ ins normal) 1)))
+
+;; some constants for lighting (would probably be uniforms in a real program)
+(defconstant +ambient-color+ (vec3 0.0 0.024 0.06) :vec3)
+(defconstant +diffuse-color+ (vec3 0.6 0.4 0.2)#++(vec3 0.2 0.4 0.6) :vec3)
+(defconstant +specular-exponent+ 16 :float)
+(defconstant +light-position+ (vec3 4 4 -5) :vec3)
+
+;; generic vertex shader used for a few lighting models
+(defun lighting-vertex ()
+  (setf gl-position (* mvp position))
+  (setf (@ outs normal) (* (mat3 normal-matrix) normal)
+        (@ outs position) (* mv position)
+        ;; interpolated lighting parameters
+        (@ outs light-direction) (- +light-position+ (vec3 (* mv position)))
+        (@ outs eye-direction) (- (vec3 (* mv position)))))
+
+(defun simple-lighting-fragment ()
+  ;; normalize the interpolated normal, since interpolation doesn't
+  ;; preserve length
+  (let* ((normal (normalize (@ ins normal)))
+         ;; same for eye direction and light direction
+         (eye-direction (normalize (@ ins eye-direction)))
+         (light-direction (normalize (@ ins light-direction)))
+         ;; calculate some intermediate values
+         (l-dot-n (clamp (dot light-direction normal) 0 1))
+         (r (reflect (- light-direction) normal))
+         (r-dot-v (clamp (dot r eye-direction) 0 1))
+         (distance (length (@ ins eye-direction))))
+    (setf color (vec4 (+ +ambient-color+
+                         (* (/ 2 distance)
+                            (+ (* +diffuse-color+ l-dot-n)
+                               (pow r-dot-v +specular-exponent+))))
+                      1))))
+
+
+;; add some random hacks to the lighting to make it look shinier
+;; and factor out the lighting calculation so it can be reused
+(defun shiny-lighting (normal eye-dir light-dir distance)
+  (let* ((l-dot-n (clamp (dot light-dir normal) 0 1))
+         (r (reflect (- light-dir) normal))
+         (r-dot-v (clamp (dot r eye-dir) 0 1))
+         ;; 0 when normal is perpendicular to view, used to fake
+         ;; lambertian reflection
+         (edge (dot eye-dir normal))
+         ;; factor out the specular calculation, and color the light a bit
+         (spec (* (vec3 1 0.4 0.4)
+                  (pow r-dot-v +specular-exponent+))))
+    ;; make a sharp drop in specular highlight so it looks like
+    ;; shinier reflection of a larger light instead of point
+    (if (> (length spec) 0.8)
+        (setf spec (* 0.8 (normalize spec)))
+        (setf spec (* spec spec)))
+    (return (vec4 (+ +ambient-color+
+                     (* (/ 2 distance)
+                        (+ (* +diffuse-color+
+                              l-dot-n)
+                           spec))
+                     ;; fake lambert term
+                     ;; (everything is reflective as you approach 0 angle)
+                     (if (< edge 0.4)
+                         (* (clamp (- 1 (expt (/ edge 0.4) 2)) 0.0 1.0)
+                            (vec3 0.0 0.15 0.25))
+                         (vec3 0)))
+                  1))))
+
+(defun shiny-fragment ()
+  (let* ((normal (normalize (@ ins normal)))
+         (eye-direction (normalize (@ ins eye-direction)))
+         (light-direction (normalize (@ ins light-direction)))
+         (distance (length (@ ins eye-direction))))
+    (setf color
+          (shiny-lighting normal eye-direction light-direction distance))))
+
+;; 4d perlin noise from "Efficient computational noise in GLSL"
+;; https://github.com/ashima/webgl-noise/blob/master/src/noise4D.glsl
+;; (original is MIT license)
+
+(defun mod289 (x)
+  (return (- x (* 289 (floor (* x (/ 289)))))))
+
+(defun permute (x)
+  (return (mod289 (* x (+ 1 (* x 34))))))
+
+(defun grad4 (j ip)
+  (let ((ones (ivec4 1 1 1 -1))
+        (p)
+        (s))
+    (setf (.xyz p) (- (* (floor (* (fract (* (vec3 j) (.xyz ip))) 7))
+                         (.z ip))
+                      1)
+          (.w p) (- 1.5 (dot (abs (.xyz p)) (.xyz ones)))
+          s (vec4 (less-than p (vec4 0)))
+          (.xyz p) (+ (.xyz p) (* (1- (* (.xyz s) 2))
+                                  (.www s))))
+    (return p)))
+
+(defconstant +f4+ 0.309016994374947451 :float)
+
+(defun snoise (v)
+  (let* ((c (vec4 0.138196601125011 ; (5 - sqrt(5))/20 G4
+                  0.276393202250021 ; 2 * G4
+                  0.414589803375032 ; 3 * G4
+                  -0.447213595499958)) ; -1 + 4 * G4
+         ;; first corner
+         (i (floor (+ v (dot v (vec4 +f4+)))))
+         (x0 (+ (- v i) (dot i (.xxxx c))))
+         ;; other corners
+         (i0)
+         (is-x (step (.yzw x0) (.xxx x0)))
+         (is-yz (step (.zww x0) (.yyz x0))))
+    (setf (.x i0) (+ (.x is-x) (.y is-x) (.z is-x))
+          (.yzw i0) (- 1 is-x))
+    (incf (.y i0) (+ (.x is-yz) (.y is-yz)))
+    (incf (.zw i0) (- 1 (.xy is-yz)))
+    (incf (.z i0) (.z is-yz))
+    (incf (.w i0) (- 1.0 (.z is-yz)))
+    ;; i0 now contains the unique values 0,1,2,3 in each channel
+    (let* ((i3 (clamp i0 0.0 1.0))
+           (i2 (clamp (- i0 1.0) 0.0 1.0))
+           (i1 (clamp (- i0 2.0) 0.0 1.0))
+           (x1 (+ (- x0 i1) (.xxxx C)))
+           (x2 (+ (- x0 i2) (.yyyy C)))
+           (x3 (+ (- x0 i3) (.zzzz C)))
+           (x4 (+ x0 (.wwww C)))
+           (i (mod289 i))
+           (j0 (permute (+ (permute (+ (permute (+ (permute (.w i))
+                                                   (.z i)))
+                                       (.y i)))
+                           (.x i))))
+           (j1 (macrolet ((p-i* (p z)
+                            `(permute (+ ,@(when p (list p))
+                                         (,z i)
+                                         (vec4 (,z i1) (,z i2)
+                                               (,z i3) 1.0)))))
+                 (p-i* (p-i* (p-i* (p-i* nil .w) .z) .y) .x)))
+           (ip (vec4 (/ 294.0) (/ 49.0) (/ 7.0) 0.0))
+           (p0 (grad4 j0 ip))
+           (p1 (grad4 (.x j1) ip))
+           (p2 (grad4 (.y j1) ip))
+           (p3 (grad4 (.z j1) ip))
+           (p4 (grad4 (.w j1) ip))
+           (norm (inverse-sqrt (vec4 (dot p0 p0)
+                                     (dot p1 p1)
+                                     (dot p2 p2)
+                                     (dot p3 p3)))))
+      (setf p0 (* p0 (.x norm))
+            p1 (* p1 (.y norm))
+            p2 (* p2 (.z norm))
+            p3 (* p3 (.w norm))
+            p4 (* p4 (inverse-sqrt (dot p4 p4))))
+      (let* ((m0 (max (- 0.6 (vec3 (dot x0 x0) (dot x1 x1) (dot x2 x2)))
+                      0.0))
+             (m1 (max (- 0.6 (vec2 (dot x3 x3) (dot x4 x4)))
+                      0.0)))
+        (setf m0 (* m0 m0)
+              m1 (* m1 m1))
+        (return (* 49 (+ (dot (* m0 m0) (vec3 (dot p0 x0)
+                                              (dot p1 x1)
+                                              (dot p2 x2)))
+                         (dot (* m1 m1) (vec2 (dot p3 x3)
+                                              (dot p4 x4))))))))))
+
+;; now make a fragment shader that uses perlin noise to distort the normals
+(defun noisy-shiny-fragment ()
+  (let* ((normal (@ ins normal))
+         (eye-direction (normalize (@ ins eye-direction)))
+         (light-direction (normalize (@ ins light-direction)))
+         (distance (length (@ ins eye-direction)))
+         ;; calculate 3 octaves of noise based on fragment world
+         ;; position and time
+         (noise (+ (snoise (vec4 (* 2 (vec3 (@ ins position)))
+                                 (/ time 5)))
+                   (* 0.5
+                      (snoise (vec4 (* 5.1 (vec3 (@ ins position)))
+                                    (/ time 4))))
+                   (* 0.2
+                      (snoise (vec4 (* 8.1 (vec3 (@ ins position)))
+                                    (/ time 2))))))
+         ;; get derivatives of noise function
+         ;; (probably should calculate directly and/or tweak glsl
+         ;;  sampling/interpolation settings, but works well enough
+         ;;  for an example)
+         (dx (dfdx noise))
+         (dy (dfdy noise)))
+    ;; push the normals a bit
+    (incf (.x normal) (* 4 dx))
+    (incf (.y normal) (* 4 dy))
+    ;; and calculate lighting based on distorted normal
+    (setf color (shiny-lighting (normalize normal)
+                                eye-direction light-direction distance))
+    (setf (.a color) 1)))
---
+++ example.lisp
@@ -0,0 +1,188 @@
+(defpackage #:3bgl-shader-example
+  (:use #:cl))
+
+(in-package #:3bgl-shader-example-shaders)
+;; combinations of shaders we will be using
+(cl:defparameter 3bgl-shader-example::*programs*
+  '(() ;; skip an entry to use for fixed-function
+    (minimal-vertex minimal-fragment)
+    (normal-vertex normal-fragment)
+    (lighting-vertex simple-lighting-fragment)
+    (lighting-vertex shiny-fragment)
+    (lighting-vertex noisy-shiny-fragment)))
+
+(in-package #:3bgl-shader-example)
+
+
+
+(defclass 3bgl-shader-example-window (glut:window)
+  ((programs :accessor programs :initform nil)
+   (index :accessor index :initform 0))
+  (:default-initargs :width 640 :height 480 :title "3bgl-shader"
+                     :mode '(:double :rgb :depth :multisample)))
+
+
+(defparameter *teapot-rotation-x* 0.0)
+(defparameter *teapot-rotation-y* 0.0)
+(defparameter *teapot-rotation-z* 0.0)
+
+(defun draw-teapot ()
+  (gl:clear-color 0 0.3 0.5 1.0)
+  (gl:clear :color-buffer :depth-buffer)
+
+  (gl:disable :blend :texture-2d)
+  (gl:enable :lighting :light0 :depth-test)
+
+  (gl:color-material :front :ambient-and-diffuse)
+
+  (gl:light :light0 :position '(0 1 1 0))
+  (gl:light :light0 :diffuse '(0.2 0.4 0.6 0))
+
+  (gl:color 1 1 1)
+  (glut:solid-teapot 1.3)
+
+  (incf *teapot-rotation-x* 0.01)
+  (incf *teapot-rotation-y* 0.05)
+  (incf *teapot-rotation-z* 0.03))
+
+
+
+
+(defparameter *modified-shader-functions* nil)
+
+(defun modified-shader-hook (modified)
+  (format t "saw modified functions ~s~%" modified)
+  (setf *modified-shader-functions*
+        (union modified *modified-shader-functions*)))
+
+(pushnew 'modified-shader-hook 3bgl-shaders::*modified-function-hook*)
+
+(defun recompile-modified-shaders (w)
+  (let* ((m *modified-shader-functions*)
+         (current-program (nth (index w) (programs w))))
+    ;; flag any shaders we are using
+    (loop for (p f names) in (programs w)
+          do (loop for n in names
+                   for i from 0
+                   when (member n m)
+                     do (setf (nth i f) t)))
+    ;; fixme: this needs a lock, since it could be modified from
+    ;; another thread
+      (setf *modified-shader-functions* nil)
+
+    (destructuring-bind (&optional program flags names)
+        current-program
+      (when (and names
+                 (or (and program (some #'identity flags))
+                     (and (not program) (every #'identity flags))))
+        (format t "~%recompiling shader program ~s for changes in functions:~&  ~a~%"
+                (index w)
+                (loop for f in flags for n in names when f collect n))
+        (setf (second current-program) (substitute nil t flags))
+        (time
+         (setf (car current-program)
+               (3bgl-shaders::reload-program (first current-program)
+                                             (first (third current-program))
+                                             (second (third current-program))
+                                             :geometry (third (third current-program))
+                                             :version 330)))))))
+
+
+(defparameter *w* nil)
+(defmethod glut:display ((w 3bgl-shader-example-window))
+  (with-simple-restart (continue "continue")
+    (setf *w* w)
+    (recompile-modified-shaders w)
+
+    (gl:with-pushed-matrix* (:projection)
+      (gl:load-identity)
+      (glu:perspective 45 (/ (glut:width w)
+                             (glut:height w))
+                       0.5 20)
+      (gl:with-pushed-matrix* (:modelview)
+        (gl:load-identity)
+
+        (flet ((radians (x) (coerce (/ (* pi x) 180) 'single-float))
+               (v (x y z) (sb-cga:vec (float x 1.0)
+                                      (float y 1.0)
+                                      (float z 1.0))))
+          (let* ((m (sb-cga:matrix*
+                     ;; some versions of glut transform the teapot
+                     ;; with gl matrix, so duplicate that here
+                     ;; (might not look right on newest freeglut though)
+                     (sb-cga:rotate-around (v 1 0 0)
+                                           (radians 270))
+                     (sb-cga:scale* (* 1.3 0.5) (* 1.3 0.5) (* 1.3 0.5))
+                     (sb-cga:translate (v 0 0 -1.5))))
+                 (v (sb-cga:matrix*
+                      (sb-cga:translate (v 0 0 -4))
+                      (sb-cga:rotate-around (v 1 0 0)
+                                            (radians *teapot-rotation-x*))
+                      (sb-cga:rotate-around (v 0 1 0)
+                                            (radians *teapot-rotation-y*))
+                      (sb-cga:rotate-around (v 0 0 1)
+                                            (radians *teapot-rotation-z*))))
+                 (p (kit.math:perspective-matrix 45
+                                                 (/ (glut:width w)
+                                                    (glut:height w))
+                                                 0.5 20))
+                 (mv (sb-cga:matrix* v m))
+                 (mvp (sb-cga:matrix* p v m))
+                 (p1 (car (nth (index w) (programs w)))))
+            (gl:load-matrix v)
+            (gl:matrix-mode :projection)
+            (gl:load-matrix p)
+            (gl:matrix-mode :modelview)
+            (when p1
+              (gl:use-program p1)
+              (3bgl-shaders::uniformf p1 "time"
+                                      (float
+                                       (/ (get-internal-real-time)
+                                          internal-time-units-per-second)))
+              (3bgl-shaders::uniform-matrix p1 "mv" mv)
+              (3bgl-shaders::uniform-matrix p1 "mvp" mvp)
+              (3bgl-shaders::uniform-matrix p1 "normalMatrix" mv))))
+
+        (draw-teapot)
+        (gl:use-program 0)))
+
+    (glut:swap-buffers)))
+
+(defmethod glut:idle ((w 3bgl-shader-example-window))
+  (glut:post-redisplay))
+
+(defmethod glut:reshape ((w 3bgl-shader-example-window) width height)
+  (setf (glut:width w) width
+        (glut:height w) height)
+  (gl:viewport 0 0 (glut:width w) (glut:height w)))
+
+(defmethod glut:keyboard ((w 3bgl-shader-example-window) key x y)
+  (declare (ignore x y))
+  (with-simple-restart
+      (continue "continue")
+    (case key
+      ((#\0 #\1 #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9)
+      (let* ((n (digit-char-p key))
+             (names (nth n *programs*)))
+        (setf (index w) n)
+        (when names
+          (unless (nth n (programs w))
+            (unless (> (length (programs w)) n)
+              (setf (programs w) (replace (make-list (1+ n)
+                                                     :initial-element nil)
+                                          (programs w))))
+            (setf (nth n (programs w))
+                  ;; for each program we store program object, list of
+                  ;; modified flags, and list of shader functions
+                  ;; (vertex, fragment, optional geometry)
+                  (list nil
+                        (make-list (length names) :initial-element t)
+                        names))
+            (format t "added program ~s: ~s~%" n (programs w)))
+          )
+        )
+      )
+     (#\Esc
+      (glut:destroy-current-window)))))
+
+;;  (glut:display-window (make-instance '3bgl-shader-example-window))
